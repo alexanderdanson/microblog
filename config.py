c98be381b1b45fb727495f1f28c6803c7c844554
Alexander Danson <alex.danson2@gmail.com>
2019-11-22 22:47:29 +0100

    language support

--- config.py
+++ config.py
@@ -12,5 +12,6 @@ class Config(object):
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     ADMINS = ['alex.danson2@gmail.com']
-    POSTS_PER_PAGE = 3
+    POSTS_PER_PAGE = 25
+    LANGUAGES = ['en', 'es']
 
